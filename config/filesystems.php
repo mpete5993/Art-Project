0b4a4ab4fbd9c39a3374909be0ea4afeb89fa47d
Bataung Mpete <mpete.bataung8@gmail.com>
2021-09-14 08:36:11 +0200

    storage links

--- config/filesystems.php
+++ config/filesystems.php
@@ -37,7 +37,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => public_path('/public'),
+            'root' => public_path('/storage'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
